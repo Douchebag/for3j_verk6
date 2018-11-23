54af4c2eaa69cd237968fcfdd25128d2ba00d6d1
i <Douchebag@users.noreply.github.com>
2018-11-23 09:13:39 +0000

    commenta ut console og taka út kommentaðan út text

--- js/filter.js
+++ js/filter.js
@@ -1,37 +1,3 @@
-/*
-(function() {                             // Lives in an IIFE
-  let imgs = document.getElementById('gallery');          // Get the images
-  let search = document.getElementById('filter-search');      // Get the input element
-  let cache = [];                         // Create an array called cache
-
-  $imgs.each(function() {                 // For each image
-    cache.push({                          // Add an object to the cache array
-      element: this,                      // This image
-      text: this.alt.trim().toLowerCase() // Its alt text (lowercase trimmed)
-    });
-  });
-
-  function filter() {                     // Declare filter() function
-    let query = this.value.trim().toLowerCase();  // Get the query
-    cache.forEach(function(img) {         // For each entry in cache pass image
-      let index = 0;                      // Set index to 0
-
-      if (query) {                        // If there is some query text
-        index = img.text.indexOf(query);  // Find if query text is in there
-      }
-
-      img.element.style.display = index === -1 ? 'none' : '';  // Show / hide
-    });
-  }
-
-  if ('oninput' in $search[0]) {          // If browser supports input event
-    $search.on('input', filter);          // Use input event to call filter()
-  } else {                                // Otherwise
-    $search.on('keyup', filter);          // Use keyup event to call filter()
-  }
-
-}());
-*/
 let images = [
     {
     name:"rabbit",
@@ -108,7 +74,7 @@ function filter() {                     // Declare filter() function
   let query = this.value.trim().toLowerCase();  // Get the query
 
   for (let oneImg of currentImgs) {
-    console.log(oneImg);
+    //console.log(oneImg);
     if (oneImg.alt.includes(query) === true){
       oneImg.hidden = false;
     } else {
